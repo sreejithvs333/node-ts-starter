e52bc34fbbf720f1ba2f90f32efd7f20cbcc1ef6
sree <sreejithvs333@gmail.com>
2023-04-10 17:40:52 +0530

    add prettier & eslint

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # node-ts-starter
+
 A simple Nodejs starter project which uses typescrip &amp; express
 
 ## Working Endpoint
+
 http://localhost:3000/api/v0/feeds
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import routes from './routes/index.router';
+import express from "express";
+import routes from "./routes/index.router";
 const app = express();
 const port = 3000;
 
-app.use("/api",routes);
+app.use("/api", routes);
 
-app.listen(port, ()=> {
-    console.log(`Server started and running on http://localhost:${port}`); 
-})
+app.listen(port, () => {
+  console.log(`Server started and running on http://localhost:${port}`);
+});
