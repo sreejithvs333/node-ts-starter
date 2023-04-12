142b671ab5ba7c0adad6e52137e597be05badddb
sree <sreejithvs333@gmail.com>
2023-04-12 21:18:48 +0530

    update endpoint

--- src/routes/api/v0/feed/feeds.router.ts
+++ src/routes/api/v0/feed/feeds.router.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { FeedStore } from "./models/feed";
 const feeds = express.Router();
+const { POSTGRES_PASSWORD } = process.env;
 
-// returns hello!
-feeds.get("/", (req: express.Request, res: express.Response) => {
-  res.status(200).send("Hello!");
+feeds.get("/", async (req: express.Request, res: express.Response) => {
+  const store = new FeedStore();
+  try {
+    const result = await store.index();
+    res.status(200).send(result);
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(400).send(err.message);
+    } else {
+      res.status(400).send(String(err));
+    }
+  }
 });
 
 export default feeds;
