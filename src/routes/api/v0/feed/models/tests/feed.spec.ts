4d95be3bb013053193a8ab74b354282f8f311389
sree <sreejithvs333@gmail.com>
2023-04-13 10:57:37 +0530

    add model tests

---
+++ src/routes/api/v0/feed/models/tests/feed.spec.ts
@@ -0,0 +1,12 @@
+import { Feed, FeedStore } from "../feed";
+
+const feedStore = new FeedStore();
+describe("Feed modal", () => {
+  it("should have an index method", () => {
+    expect(feedStore.index).toBeDefined();
+  });
+  it("index() should return a list of products", async () => {
+    const result = await feedStore.index();
+    expect(result).toEqual([]);
+  });
+});
