b67dd99d2a39f6f43ee1d2c742097e0b8649b4ac
sree <sreejithvs333@gmail.com>
2023-04-13 21:03:24 +0530

    update database file

--- src/database.ts
+++ src/database.ts
@@ -3,13 +3,27 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_TEST_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
+  process.env;
 
-const Client = new Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD
-});
+let Client;
+
+if (ENV === "test") {
+  Client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD
+  });
+}
+
+if (ENV === "dev") {
+  Client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD
+  });
+}
 
 export default Client;
