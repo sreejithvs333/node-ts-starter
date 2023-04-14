57dc68c90e3bf8db910159b638b1b6e9eb53f0e7
sree <sreejithvs333@gmail.com>
2023-04-14 09:29:48 +0530

    add more model methods

--- src/routes/api/v0/feed/models/feed.ts
+++ src/routes/api/v0/feed/models/feed.ts
@@ -17,4 +17,56 @@ export class FeedStore {
       throw new Error(`Cannot get feeds ${err}`);
     }
   }
+
+  async show(id: number): Promise<Feed> {
+    try {
+      const connection = await Client.connect();
+      const sql = `SELECT * FROM feeds WHERE id=($1)`;
+      const result = await connection.query(sql, [id]);
+      connection.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Cannot find feed with id=${id}. Error: ${err}`);
+    }
+  }
+
+  async create(feed: Feed): Promise<Feed> {
+    try {
+      const connection = await Client.connect();
+      const sql = `INSERT INTO feeds(title,description) VALUES ($1,$2) RETURNING *`;
+      const result = await connection.query(sql, [feed.title, feed.description]);
+      connection.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Cannot add new feed with title=${feed.title}. Error: ${err}`);
+    }
+  }
+
+  async update(feed: Feed, fieldsToUpdate: (keyof Feed)[]): Promise<Feed> {
+    try {
+      const connection = await Client.connect();
+      const sql = {
+        text: `UPDATE feeds SET ${fieldsToUpdate
+          .map((field, i) => `${field} = $${i + 1}`)
+          .join(", ")} WHERE id = $${fieldsToUpdate.length + 1}`,
+        values: [...fieldsToUpdate.map((field) => feed[field]), feed.id]
+      };
+      const result = await connection.query(sql);
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Couldn't update feed with id=${feed.id}. Error: ${err}`);
+    }
+  }
+
+  async delete(id: number): Promise<Feed> {
+    try {
+      const connection = await Client.connect();
+      const sql = `DELETE FROM feeds WHERE id=($1)`;
+      const result = await connection.query(sql, [id]);
+      connection.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Couldn't delete feed with id=${id}. Error: ${err}`);
+    }
+  }
 }
