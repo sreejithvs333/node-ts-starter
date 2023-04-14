2e01a8924b5efc4e7f96aff354bb357b032ee287
sree <sreejithvs333@gmail.com>
2023-04-14 13:47:27 +0530

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import routes from "./routes/index.router";
+import cors from "cors";
 const app = express();
 const port = 3000;
 
+const corsOptions: cors.CorsOptions = {
+  // white listing all domain. Replace it with client domain
+  origin: "*",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", routes);
 
